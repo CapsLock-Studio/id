298f4cc822edd26cd361d5163ea2ffd6c7429334
Michael Kuan <michael34435@capslock.tw>
2020-09-23 16:40:56 +0800

    chore: grab id from __a=1

--- src/routes/ig.js
+++ src/routes/ig.js
@@ -1,22 +1,14 @@
-/* eslint-disable no-eval */
 const _ = require('lodash');
 const axios = require('axios');
-const cheerio = require('cheerio');
 
 module.exports = {
   async show(ctx) {
     const alias = _.get(ctx, 'params.id');
-    const data = await axios.get(`https://www.instagram.com/${alias}`);
-    const $ = cheerio.load(data.data);
-    const script = $('script:contains("window._sharedData = ")').html();
+    const data = await axios.get(`https://www.instagram.com/${alias}?__a=1`);
+    const user = _.get(data, 'data.graphql.user.id');
 
-    if (script) {
-      const window = {};
-
-      // eval script here
-      eval(script);
-
-      ctx.body = _.get(window, '_sharedData.entry_data["ProfilePage"][0].graphql.user.id');
+    if (user) {
+      ctx.body = user;
     } else {
       ctx.status = 404;
     }
