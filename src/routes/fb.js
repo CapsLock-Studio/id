f0ee13fbac6b7ce17000a274db7b54e66570b8b7
Calvin Huang <calvin.peak@capslock.tw>
2020-11-17 15:55:45 +0800

    fix: facebook remove the meta from the page since they're upgrading to the new UI

--- src/routes/fb.js
+++ src/routes/fb.js
@@ -6,18 +6,42 @@ const { URL } = require('url');
 module.exports = {
   async show(ctx) {
     const alias = _.get(ctx, 'params.id');
-    const data = await axios.get(`https://www.facebook.com/${alias}`, { ...ctx.socks5 });
-    const $ = cheerio.load(data.data);
-    const ios = $('meta[property="al:ios:url"]').attr('content');
+    const urlString = `https://www.facebook.com/${alias}`;
 
-    if (ios) {
-      const url = new URL(ios);
-      const id = url.searchParams.get('id');
-      const pathname = url.pathname.split('/').slice(-1).pop();
+    const url = new URL(urlString);
 
-      ctx.body = _.defaultTo(id, pathname);
-    } else {
-      ctx.status = 404;
+    // The 404 or other network errors would be thrown by axios.
+    const { data } = await axios.get(urlString, {
+      ...ctx.socks5,
+      headers: {
+        authority: 'www.facebook.com',
+        path: encodeURI(url.pathname),
+        method: 'get',
+        accpet: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'accept-language': 'en-US,en;q=0.9',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
+        'cache-control': 'max-age=0',
+        'upgrade-insecure-requests': 1,
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+      },
+    });
+    const $ = cheerio.load(data);
+
+    const pageletDOM = $('[id^="PagePagesLikedByPageSecondaryPagelet_"]');
+    if (pageletDOM) {
+      const [, id] = /PagePagesLikedByPageSecondaryPagelet_(\d+)/g.exec(pageletDOM.attr('id')) || [];
+
+      if (id) {
+        ctx.body = id;
+        return;
+      }
+
+      throw new Error('Decode from ID attr failed, the page ID regex needs to be updated');
     }
+
+    throw new Error('Decode from DOM [id^="PagePagesLikedByPageSecondaryPagelet_"] failed, might be unknown page layout');
   },
 };
